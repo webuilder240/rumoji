e85176d2c0d8bdbc1e9106ef5bd49ae23fca84a4
Shall1230 <fujitashunsuke@osiro.it>
2025-07-11 06:16:31 +0000

    add combined airplane

--- lib/rumoji/emoji/combine.rb
+++ lib/rumoji/emoji/combine.rb
@@ -7,6 +7,7 @@ module Rumoji
   class Emoji
     COMBINE = SortedSet[
       self.new("\u{2764 FE0F 200D 1F525}", [:heart_on_fire]),
+      self.new("\u{2708 FE0F}", [:airplane]), 
     ]
   end
 end
